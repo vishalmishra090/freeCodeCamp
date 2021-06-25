c60e8c52956517e5e0269fa8da44e20e99a8d59c
Vishal Mishra <vishalmishra090@gmail.com>
2021-06-25 15:15:01 +0530

    sanitize html using dompurify

--- Front End Development Libraries/markdown_previewer/src/components/PreviewBox.jsx
+++ Front End Development Libraries/markdown_previewer/src/components/PreviewBox.jsx
@@ -4,28 +4,32 @@ import "highlight.js/styles/github.css";
 import useTheme from '../hooks/useTheme';
 import useMarkdown from '../hooks/useMarkdown';
 import  marked from 'marked'
-import parse from 'html-react-parser';
 import hljs from "highlight.js";
+import DOMpurify from 'dompurify'
+import parse from 'html-react-parser'
+
+
 
 marked.setOptions({
-    highlight: function (code, lang) {
+    highlight: function (code, lang) { 
       const language = hljs.getLanguage(lang) ? lang : "plaintext";
       return hljs.highlight(code, { language }).value;
     },
     breaks: true
 });
 
+
+
 function PreviewBox() {
     let {theme} = useTheme()
     let {markdown} = useMarkdown()
-    
     return (
         <div id="previewBox" style={{...theme.previewBox}}>
             <div id="previewHeader">
                 Preview
             </div>
             <div id="preview" className="markdown-body">
-               {parse(marked(markdown),{ trim: true })}
+              {parse(DOMpurify.sanitize(marked(markdown)))}
             </div>
         </div>
     )
